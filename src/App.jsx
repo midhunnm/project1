c6600995a2f2546324b1fcff24ae9dcdfc79bae8
midhunnm <midhunnm69@gmail.com>
2025-06-22 19:35:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import React from 'react'
-import Nav from './Components/Nav'
-import { Route, Routes } from 'react-router-dom'
-import Login from './Components/login'
-import Signup from './Components/signup'
-import './App.css'
-const App = () => {
+import React from 'react';
+import Nav from './Components/Nav';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Components/login';
+import Signup from './Components/signup';
+import './App.css';
+import Cart from './Components/Cart';
+import Home from './Components/Home';
 
+const App = () => {
   return (
     <div className="app-background">
       <Nav />
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='login' element={<Login />} />
         <Route path='signup' element={<Signup />} />
+        <Route path='cart' element={<Cart />} />
       </Routes>
-      <div className="image-container">
-        <img
-          src="bg1.jpg"
-          alt="Wood Background"
-          className="background-image"
-        />
-        <div className="image-text">
-          <div className="main-text">Going eco has never been Easier</div>
-          <div className="sub-text">Make a Difference With Every Purchase, Shop Sustainable.</div>
-        </div>
-
-      </div>
-      
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
