cc2e6251cefb648cfb73bd28a8fed36783ff9790
midhunnm <midhunnm69@gmail.com>
2025-06-22 19:41:42 +0530

    Update Home.jsx midhun

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,1 +1,35 @@
 
+import React from 'react';
+import './Home.css';
+import products from '../data/product';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      {/* Background image and text */}
+      <div className="image-container">
+        <img src="bg1.jpg" alt="Wood Background" className="background-image" />
+        <div className="image-text">
+          <div className="main-text">Going eco has never been Easier</div>
+          <div className="sub-text">Make a Difference With Every Purchase, Shop Sustainable.</div>
+        </div>
+      </div>
+
+      {/* Product Section */}
+      <div className="product-section">
+        <div className="product-grid">
+          {products.map((product) => (
+            <div className="product-card" key={product.id}>
+              <img src={product.image} alt={product.name} className="product-img" />
+              <h3>{product.name}</h3>
+              <p className="product-price">₹{product.price}</p>
+              <button className="buy-btn">Add to Cart</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
